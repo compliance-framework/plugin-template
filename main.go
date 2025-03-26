9c56fa2b03ec4bd7a91fc3fc11fcc62a7cd50b43
Steven Trotter <stevetrot@gmail.com>
2025-03-26 20:13:50 +0000

    Upgraded the plugin to latest API and agent versions

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"time"
 
-	policyManager "github.com/compliance-framework/agent/policy-manager"
 	"github.com/compliance-framework/agent/runner"
 	"github.com/compliance-framework/agent/runner/proto"
-	"github.com/compliance-framework/configuration-service/sdk"
-	"github.com/google/uuid"
+	"github.com/compliance-framework/plugin-template/internal"
 	"github.com/hashicorp/go-hclog"
 	goplugin "github.com/hashicorp/go-plugin"
-	protolang "google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type CompliancePlugin struct {
@@ -45,7 +39,6 @@ type CompliancePlugin struct {
 // The agent will:
 //   - Start the plugin
 //   - Call Configure() with teh required config
-//   - Call PrepareForEval() so the plugin can collect the relevant state
 //   - Call Eval() with the first policy bundles (one by one, in turn),
 //     so the plugin can report any violations against the configuration
 func (l *CompliancePlugin) Configure(req *proto.ConfigureRequest) (*proto.ConfigureResponse, error) {
@@ -68,177 +61,34 @@ func (l *CompliancePlugin) Eval(request *proto.EvalRequest, apiHelper runner.Api
 	// When a user passes multiple policy bundles to the agent, each will be passed to Eval in turn to run against the
 	// same data collected in PrepareForEval.
 
-	var errAcc error
-
-	data := map[string]interface{}{
-		"hello": "world",
-	}
-
 	ctx := context.TODO()
-	startTime := time.Now()
 
-	evalStatus := proto.ExecutionStatus_SUCCESS
+	activities := make([]*proto.Activity, 0)
 
-	for _, policyPath := range request.PolicyPaths {
-		// The Policy Manager aggregates much of the policy execution and output structuring.
-		results, err := policyManager.New(ctx, l.logger, policyPath).Execute(ctx, "compliance_plugin", data)
+	dataFetcher := internal.NewDataFetcher(l.logger, l.config)
 
-		if err != nil {
-			l.logger.Error("Failed to evaluate against policy bundle", "error", err)
-			evalStatus = proto.ExecutionStatus_FAILURE
-			errAcc = errors.Join(errAcc, err)
-			continue
-		}
-
-		assessmentResult := runner.NewCallableAssessmentResult()
-		assessmentResult.Title = "Plugin template"
-
-		for _, result := range results {
-
-			// There are no violations reported from the policies.
-			// We'll send the observation back to the agent
-			if len(result.Violations) == 0 {
-				title := "The plugin succeeded. No compliance issues to report."
-				assessmentResult.AddObservation(&proto.Observation{
-					Uuid:        uuid.New().String(),
-					Title:       &title,
-					Description: "The plugin policies did not return any violations. The configuration is in compliance with policies.",
-					Collected:   timestamppb.New(time.Now()),
-					Expires:     timestamppb.New(time.Now().AddDate(0, 1, 0)), // Add one month for the expiration
-					RelevantEvidence: []*proto.RelevantEvidence{
-						{
-							Description: fmt.Sprintf("Policy %v was evaluated, and no violations were found on machineId: %s", result.Policy.Package.PurePackage(), "ARN:12345"),
-						},
-					},
-					Labels: map[string]string{
-						"package": string(result.Policy.Package),
-						"type":    "template",
-					},
-				})
-
-				assessmentResult.AddFinding(&proto.Finding{
-					Title:       fmt.Sprintf("No violations found on %s", result.Policy.Package.PurePackage()),
-					Description: fmt.Sprintf("No violations found on the %s policy within the Template Compliance Plugin.", result.Policy.Package.PurePackage()),
-					Target: &proto.FindingTarget{
-						Status: &proto.ObjectiveStatus{
-							State: runner.FindingTargetStatusSatisfied,
-						},
-					},
-					Labels: map[string]string{
-						"package": string(result.Policy.Package),
-						"type":    "template",
-					},
-				})
-			}
-
-			// There are violations in the policy checks.
-			// We'll send these observations back to the agent
-			if len(result.Violations) > 0 {
-				title := fmt.Sprintf("The plugin found violations for policy %s on machineId: %s", result.Policy.Package.PurePackage(), "ARN:12345")
-				observationUuid := uuid.New().String()
-				assessmentResult.AddObservation(&proto.Observation{
-					Uuid:        observationUuid,
-					Title:       &title,
-					Description: fmt.Sprintf("Observed %d violation(s) for policy %s within the Plugin on machineId: %s.", len(result.Violations), result.Policy.Package.PurePackage(), "ARN:12345"),
-					Collected:   timestamppb.New(time.Now()),
-					Expires:     timestamppb.New(time.Now().AddDate(0, 1, 0)), // Add one month for the expiration
-					RelevantEvidence: []*proto.RelevantEvidence{
-						{
-							Description: fmt.Sprintf("Policy %v was evaluated, and %d violations were found on machineId: %s", result.Policy.Package.PurePackage(), len(result.Violations), "ARN:12345"),
-						},
-					},
-					Labels: map[string]string{
-						"package": string(result.Policy.Package),
-						"type":    "template",
-					},
-				})
-
-				for _, violation := range result.Violations {
-					assessmentResult.AddFinding(&proto.Finding{
-						Title:       violation.Title,
-						Description: violation.Description,
-						Remarks:     &violation.Remarks,
-						RelatedObservations: []*proto.RelatedObservation{
-							{
-								ObservationUuid: observationUuid,
-							},
-						},
-						Target: &proto.FindingTarget{
-							Status: &proto.ObjectiveStatus{
-								State: runner.FindingTargetStatusNotSatisfied,
-							},
-						},
-						Labels: map[string]string{
-							"package": string(result.Policy.Package),
-							"type":    "template",
-						},
-					})
-				}
-			}
-
-			for _, risk := range result.Risks {
-				links := []*proto.Link{}
-				for _, link := range risk.Links {
-					links = append(links, &proto.Link{
-						Href: link.URL,
-						Text: &link.Text,
-					})
-				}
-
-				assessmentResult.AddRiskEntry(&proto.Risk{
-					Title:       risk.Title,
-					Description: risk.Description,
-					Statement:   risk.Statement,
-					Props:       []*proto.Property{},
-					Links:       []*proto.Link{},
-				})
-			}
-		}
-
-		endTime := time.Now()
-
-		// Send the results back to the agent using the API Helper process the agent created for us
-		assessmentResult.Start = timestamppb.New(startTime)
-		assessmentResult.End = timestamppb.New(endTime)
-
-		assessmentResult.AddLogEntry(&proto.AssessmentLog_Entry{
-			Title:       protolang.String("Template check"),
-			Description: protolang.String("Template plugin checks completed successfully"),
-			Start:       timestamppb.New(startTime),
-			End:         timestamppb.New(endTime),
-		})
-
-		streamId, err := sdk.SeededUUID(map[string]string{
-			"type":   "template",
-			"policy": policyPath,
-		})
-		if err != nil {
-			evalStatus = proto.ExecutionStatus_FAILURE
-			errAcc = errors.Join(errAcc, err)
-			continue
-		}
-
-		err = apiHelper.CreateResult(
-			streamId.String(),
-			map[string]string{
-				"type": "template",
-			},
-			policyPath,
-			assessmentResult.Result(),
-		)
-
-		if err != nil {
-			l.logger.Error("Failed to add assessment result", "error", err)
-			evalStatus = proto.ExecutionStatus_FAILURE
-			errAcc = errors.Join(errAcc, err)
-		}
+	data, collectSteps, err := dataFetcher.FetchData()
+	if err != nil {
+		return &proto.EvalResponse{
+			Status: proto.ExecutionStatus_FAILURE,
+		}, fmt.Errorf("failed to fetch data: %w", err)
 	}
+
+	stepActivities := append(activities, &proto.Activity{
+		Title:       "Collect data",
+		Description: "Collect data, and prepare collected data for validation in policy engine",
+		Steps:       collectSteps,
+	})
+
+	policyEvaluator := internal.NewPolicyEvaluator(ctx, l.logger, stepActivities)
+
+	evalStatus, err := policyEvaluator.Eval(data, request.PolicyPaths)
 
 	resp := &proto.EvalResponse{
 		Status: evalStatus,
 	}
 
-	return resp, errAcc
+	return resp, err
 }
 
 func main() {
